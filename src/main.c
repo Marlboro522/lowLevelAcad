0e547cb090244e658824b4216b85548d2a4555d4
Marlboro522 <ilu.raghus@icloud.com>
2026-03-11 05:04:04 -0600

    Add remaning fucntion prototypes to kv header

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<kv.h>
-int main(int argc, char** argv) { kv_t *table = kv_init(3);
-  printf("%p is the address of the table\n", table);
-  printf("%ld is the capacity of that table\n", table->capacity);
-  return 0;
-}
+// #include<stdio.h>
+// #include<kv.h>
+// int main(int argc, char** argv) { kv_t *table = kv_init(3);
+//   printf("%p is the address of the table\n", table);
+//   printf("%ld is the capacity of that table\n", table->capacity);
+//   return 0;
+// }
